16a9f3636cc697b9985f34e91841b7ac69058e48
Alexandr Nekrasov <ale88andr@gmail.com>
2016-06-08 17:39:21 +0300

    [#0015] replace search to filter

--- equipment/views.py
+++ equipment/views.py
@@ -12,27 +12,27 @@ class EquipmentsView(ListView):
     page_title = 'Учтённое оборудование'
 
     def dispatch(self, request, *args, **kwargs):
-        self.search_form = EquipmentSearchForm(request.GET)
-        self.search_form.is_valid()
+        self.filter_form = EquipmentSearchForm(request.GET)
+        self.filter_form.is_valid()
 
-        if self.search_form.cleaned_data.get('on_page'):
-            self.paginate_by = int(self.search_form.cleaned_data.get('on_page'))
+        if self.filter_form.cleaned_data.get('on_page'):
+            self.paginate_by = int(self.filter_form.cleaned_data.get('on_page'))
 
         return super(EquipmentsView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(EquipmentsView, self).get_context_data(**kwargs)
-        context['search_form'] = self.search_form
+        context['filter_form'] = self.filter_form
         return context
 
     def get_queryset(self):
         queryset = Equipment.all()
 
-        if self.search_form.cleaned_data.get('filter_type'):
-            queryset = queryset.filter(type=self.search_form.cleaned_data.get('filter_type'))
-        if self.search_form.cleaned_data.get('filter_responsible'):
-            queryset = queryset.filter(responsible=self.search_form.cleaned_data.get('filter_responsible'))
-        if self.search_form.cleaned_data.get('sort_by'):
-            queryset = queryset.order_by(self.search_form.cleaned_data.get('sort_by'))
+        if self.filter_form.cleaned_data.get('filter_type'):
+            queryset = queryset.filter(type=self.filter_form.cleaned_data.get('filter_type'))
+        if self.filter_form.cleaned_data.get('filter_responsible'):
+            queryset = queryset.filter(responsible=self.filter_form.cleaned_data.get('filter_responsible'))
+        if self.filter_form.cleaned_data.get('sort_by'):
+            queryset = queryset.order_by(self.filter_form.cleaned_data.get('sort_by'))
 
         return queryset
