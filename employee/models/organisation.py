5b80b531078167b93d679f4659a3e54a4b0ed6fe
ale88andr <ale88andr@gmail.com>
2016-07-07 19:23:08 +0300

    [#0041] fixed user views auth

--- employee/models/organisation.py
+++ employee/models/organisation.py
@@ -5,7 +5,7 @@ from django.db import models
 class Organisation(models.Model):
     title = models.CharField(_('Наименование'), max_length=80, unique=True, db_index=True)
     short_title = models.CharField(_('Краткое наименование'), max_length=40, unique=True, null=True, blank=True)
-    parent = models.ForeignKey('Organisation', default=0, blank=True, null=True, verbose_name='В подчинении')
+    parent = models.ForeignKey('self', related_name='subordinates', default=0, blank=True, null=True, verbose_name='В подчинении')
 
     @staticmethod
     def all():
@@ -16,13 +16,10 @@ class Organisation(models.Model):
         return Organisation.objects.get(pk=pk)
 
     def has_subordinates(self):
-        if len(self.subordinates()):
+        if self.subordinates.count():
             return True
 
         return False
-
-    def subordinates(self):
-        return Organisation.all().filter(parent=self.pk)
 
     def __str__(self):
         return self.title
