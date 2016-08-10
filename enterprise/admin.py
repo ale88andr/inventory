60dca72f50e6b7f5fd823cdf30094e92aba3b9d8
Alexandr Nekrasov <ale88andr@gmail.com>
2016-08-10 16:57:58 +0300

    [#0056] Improove admin interface objects

--- enterprise/admin.py
+++ enterprise/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
 
-from enterprise.models import *
+from enterprise.models import (
+    Organisation,
+    Location,
+    Department,
+    Employee
+)
 
-admin.site.register([Organisation, Location, Employee, Department])
+
+@admin.register(Employee)
+class EmployeeBackend(admin.ModelAdmin):
+    search_fields = ['surname', 'position']
+    readonly_fields = ('created_at',)
+    save_as = True
+    list_display = ('__str__', 'location', 'position', 'department', 'organisation')
+    list_filter = ('position', 'organisation', 'state')
+
+
+@admin.register(Organisation)
+class OrganisationBackend(admin.ModelAdmin):
+    search_fields = ['title', 'short_title']
+    list_display = ('title', 'parent')
+    empty_value_display = 'Нет'
+    list_filter = ('parent', )
+
+
+@admin.register(Department)
+class DepartmentBackend(admin.ModelAdmin):
+    list_filter = ('organisation', )
+    list_display = ('__str__', 'organisation')
+
+
+@admin.register(Location)
+class LocationBackend(admin.ModelAdmin):
+    search_fields = ['emplacement', ]
+    list_display = ('emplacement', 'is_active', 'organisation')
+    list_filter = ('organisation', 'is_active')
--- equipment/admin.py
+++ equipment/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
-
 from equipment.models import Equipment, EquipmentTypes
 
-admin.site.register([Equipment, EquipmentTypes])
+
+@admin.register(Equipment)
+class EquipmentBackend(admin.ModelAdmin):
+    empty_value_display = 'Отсутствует'
+    fieldsets = (
+        ('Основные данные', {
+            'fields': ('type', 'model', 'serial_number', 'inventory_number', 'responsible', 'created_at')
+        }),
+        ('Дополнительно', {
+            'classes': ('collapse',),
+            'fields': ('revised_at', 'note', ('is_used', 'is_repair')),
+        }),
+    )
+    list_display = ('model', 'type', 'inventory_number', 'is_used', 'responsible', 'revised_at')
+    list_filter = ('type', 'responsible', 'model', 'is_used', 'is_repair')
+    list_per_page = 25
+    list_select_related = ('responsible', 'type')
+    readonly_fields = ('created_at',)
+    save_as = True
+    search_fields = ['model', 'inventory_number', 'serial_number']
+
+
+@admin.register(EquipmentTypes)
+class EquipmentTypesBackend(admin.ModelAdmin):
+    search_fields = ['value']
+
+
 admin.site.site_header = 'Инвентаризация: администрирование сервиса'
